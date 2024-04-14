3a0735a1ec979bd5ee13d878292a10cfffccac20
Suther1and <aiur272@gmail.com>
2024-04-14 22:55:27 +0300

    sprint_05
    small fixes to border color

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -112,6 +112,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
             guard let self = self else {return}
             model.completion?()
             questionFactory.requestNextQuestion()
+            self.imageView.layer.borderColor = UIColor.clear.cgColor
 
         }
         alert.addAction(action)
@@ -196,7 +197,6 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
             
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
-            self.imageView.layer.borderColor = UIColor.clear.cgColor
             self.changeStateButtons(isEnabled: true)
         } else {
             currentQuestionIndex += 1
@@ -224,67 +224,3 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
 
 }
 
-
-
-
-//MARK: -Mock Data
-/*
- Картинка: The Godfather
- Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Dark Knight
- Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: Kill Bill
- Настоящий рейтинг: 8,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Avengers
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: Deadpool
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Green Knight
- Настоящий рейтинг: 6,6
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: Old
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- 
- 
- Картинка: The Ice Age Adventures of Buck Wild
- Настоящий рейтинг: 4,3
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- 
- 
- Картинка: Tesla
- Настоящий рейтинг: 5,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- 
- 
- Картинка: Vivarium
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- */
