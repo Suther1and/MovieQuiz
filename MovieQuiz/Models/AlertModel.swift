5cf2385c084b548daffec8cfee3f98b1ecef5b13
Suther1and <aiur272@gmail.com>
2024-04-14 16:15:45 +0300

    sprint_05
    AlertModel/AlertPresenter files created

---
+++ MovieQuiz/Models/AlertModel.swift
@@ -0,0 +1,16 @@
+//
+//  AlertModel.swift
+//  MovieQuiz
+//
+//  Created by Pavel Barto on 14.04.2024.
+//
+
+import UIKit
+
+
+struct AlertModel {
+    let title: String
+    let message: String
+    let buttonText: String
+    let completion: UIAlertAction
+}
---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  AlertPresenter.swift
+//  MovieQuiz
+//
+//  Created by Pavel Barto on 14.04.2024.
+//
+
+import UIKit
